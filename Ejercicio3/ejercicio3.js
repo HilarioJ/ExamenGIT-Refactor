92d818abce2e85c0646aba48b351bfdf50999fdf
Hilario <hilariojosebandressasal@fp.iesromerovargas.com>
2022-05-06 09:28:15 +0200

    Ponemos todas las funciones dentro de grupo

--- Ejercicio3/ejercicio3.js
+++ Ejercicio3/ejercicio3.js
@@ -2,35 +2,30 @@ function Grupo (codigo, horario) {
     this.codigo = codigo;
     this.horario = horario;
     this.alumnos = [];
-    
-    this.matricular = function matricular (dni, nombre, nota) {
-    this.alumnos.push (Crea (dni, nombre, nota));
+    this.numeroAlumnos = function NumAlumnos(){
+     let num = this.alumnos.length;
+     return num;
     }
     
-    function Crea(dni, nombre, nota) {
+    this.matricular = function matricular (dni, nombre, nota) {
+    this.alumnos.push (Alumno (dni, nombre, nota));
+    
+    this.media = function media() {
+     let suma = 0;
+     for (let i=0; i < this.alumnos.length; i++ ){
+           suma += this.alumnos[i].nota;
+      }
+      return (suma/ this.alumnos.length);
+    }
+    }
+    
+    function Alumno(dni, nombre, nota) {
         this.dni = dni;
         this.nombre = nombre;
         this.nota = nota;
     }
-    
-    grupo.num = function Alumnos () {
-         let num = this.alumnos.length;
-         return num;
-    };
-    
-    grupo.media = function Media () {
-         let suma = 0;
-         for (let i=0; i < this.alumnos.length; i++ ){
-               suma += this.alumnos[i].nota;
-          }
-          return (suma/ this.alumnos.length);
-    };
-    
-    return grupo;
-    };
-    
-    let asir1 = Grupo("ASIR1", "tarde");
+       let asir1 = Grupo("ASIR1", "tarde");
     
     asir1.matricular("2345", "pepe", 7.8);
     asir1.matricular("4545", "juan", 9.8);
-     
+}
