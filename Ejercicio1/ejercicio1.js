e59799e9b83c36266beb1c0279d91b9e2c00020e
Hilario <hilariojosebandressasal@fp.iesromerovargas.com>
2022-05-06 10:32:31 +0200

    Creacion de varaible más explicativa

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -2,41 +2,44 @@ function Calcular() {
 
     const sueldo = parseInt(document.getElementById('sueldo').value);
     const antiguedad = parseInt(document.getElementById('antiguedad').value);
-     let aumento = CalcularSueldo(sueldo, antiguedad);
+     let aumento = CalcularAumento(sueldo, antiguedad);
      resultado.innerHTML = "El sueldo es " + aumento + "€";
 
 }
 
-function CalcularSueldo(sueldo, antiguedad){
+function CalcularAumento(sueldo, antiguedad){
     let aumento = 0;
+    let porcentaje;
     if (sueldo < 1000) {
         if (antiguedad > 10) {
-             aumento = sueldo * 1.20;
+             porcentaje = 1.20;
             
         }
         else {
             if (antiguedad > 5) {
-                aumento = sueldo * 1.15;
+                porcentaje =  1.15;
             }
             else {
-                aumento = sueldo * 1.10;
+                porcentaje =  1.10;
             }
         }
     }
     else {
         if (antiguedad > 10) {
-            aumento = sueldo * 1.10;
+            porcentaje =  1.10;
         }
         else {
             if (antiguedad > 5) {
-                aumento = sueldo * 1.05;
+                porcentaje =  1.05;
             }
             else {
-                aumento = sueldo * 1.01;
+                porcentaje =  1.01;
             }
         }
     }
+    
+    aumento = sueldo * porcentaje;
 
-    return aumento;
-   
+    return aumento;   
+
 }
--- Ejercicio3/ejercicio3.js
+++ Ejercicio3/ejercicio3.js
@@ -1,10 +1,10 @@
-function Grupo (codigo, horario) {
+function grupo (codigo, horario) {
     this.codigo = codigo;
     this.horario = horario;
     this.alumnos = [];
     this.numeroAlumnos = this.alumnos.length;        
     this.matricularAlumno = function matricular(dni, nombre, nota){
-         this.alumnos.push (Alumno (dni, nombre, nota))
+         this.alumnos.push (alumno (dni, nombre, nota))
     }
     
     this.media = function media() {
@@ -16,12 +16,12 @@ function Grupo (codigo, horario) {
     }
       
     
-    function Alumno(dni, nombre, nota) {
+    function alumno(dni, nombre, nota) {
         this.dni = dni;
         this.nombre = nombre;
         this.nota = nota;
     }
     
-       let asir1 = Grupo("ASIR1", "tarde".matricularAlumno("2345","pepe", 7.8).matricularAlumnos("4545", "juan", 9.8));
+       let asir1 = grupo("ASIR1", "tarde".matricularAlumno("2345","pepe", 7.8).matricularAlumnos("4545", "juan", 9.8));
 
 }
