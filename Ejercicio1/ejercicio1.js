7753a6defcfb225c5f673241c5c9bbbed8b7ff3f
Hilario <hilariojosebandressasal@fp.iesromerovargas.com>
2022-05-06 08:40:17 +0200

    Quito código repetido

--- Ejercicio1/ejercicio1.js
+++ Ejercicio1/ejercicio1.js
@@ -2,37 +2,33 @@ function Calcular() {
 
     const sueldo = parseInt(document.getElementById('sueldo').value);
     const antiguedad = parseInt(document.getElementById('antiguedad').value);
-
+    let aumento = 0;
     if (sueldo < 1000) {
         if (antiguedad > 10) {
-            let aumento = sueldo * 1.20;
-            resultado.innerHTML = "El sueldo es " + aumento + "€";
+             aumento = sueldo * 1.20;
+            
         }
         else {
             if (antiguedad > 5) {
-                let aumento = sueldo * 1.15;
-                resultado.innerHTML = "El sueldo es " + aumento + "€";
+                aumento = sueldo * 1.15;
             }
             else {
-                let aumento = sueldo * 1.10;
-                resultado.innerHTML = "El sueldo es " + aumento + "€";
+                aumento = sueldo * 1.10;
             }
         }
     }
     else {
         if (antiguedad > 10) {
-            let aumento = sueldo * 1.10;
-            resultado.innerHTML = "El sueldo es " + aumento + "€";
+            aumento = sueldo * 1.10;
         }
         else {
             if (antiguedad > 5) {
-                let aumento = sueldo * 1.05;
-                resultado.innerHTML = "El sueldo es " + aumento + "€";
+                aumento = sueldo * 1.05;
             }
             else {
-                let aumento = sueldo * 1.01;
-                resultado.innerHTML = "El sueldo es " + aumento + "€";
+                aumento = sueldo * 1.01;
             }
         }
     }
+    resultado.innerHTML = "El sueldo es " + aumento + "€";
 }
